e87954dc41039767d011dbd0f1289db59a79b92e
Juan Camilo Acosta Arango <ja0335@gmail.com>
2016-08-27 17:54:07 -0500

    Added project Boids and Frost Giant

---
+++ Projects/Boids/scripts/Export_sym.py
@@ -0,0 +1,16 @@
+import os;
+import os.path;
+import sys;
+n = hou.node("/obj/box/solver1");
+for i in xrange(1, 5000):
+    print "Start writing frame " + str(i) + "...\n";
+    Str = "C:\\Simulation\\Position\\frame_" + str(i) + ".txt";
+    f = open(Str, "w");
+    for point in n.geometryAtFrame(i).points():
+        data = point.attribValue("P");
+        Str = str(data[0]) + "," + str(data[1]) + "," + str(data[2]) + "\n";
+        f.write( Str );
+    f.close();
+    print "End writing frame " + str(i) + "\n";
+
+
